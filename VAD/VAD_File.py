1f6fe17538c51241f6187bd3d9471bf789e5f60b
unknown <jrruaya1@up.edu.ph>
2022-11-30 22:01:13 +0800

    1.0 Requirements.txt and base files

---
+++ VAD/VAD_File.py
@@ -0,0 +1,10 @@
+import pvcobra
+
+def hello():
+    print("Hello")
+
+def handleVAD():
+    handle = pvcobra.create(access_key = "QDSbvyxt9Wg7uxOmdoa7Pi96CqjPHSek+Cxq2cTZxmuZY13MHvFVwA==")
+
+
+handleVAD()
---
+++ assistant.py
@@ -0,0 +1,9 @@
+import VAD_File
+
+def main():
+    print("Starting personal assistant...")
+    hello()
+    return 
+
+if __name__ == "__main__":
+    main()
